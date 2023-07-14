5f63a98e2e2d12f052cdef074ad758d30eb4a074
Herbert Santos <herbert.santos@LISMAR.COM.BR>
2023-07-14 12:51:15 -0300

    Firebase Trabalhando com Lista

--- src/App.js
+++ src/App.js
@@ -1,91 +1,119 @@
 import { useState } from 'react'
 import { db } from './firebaseConnection';
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore'
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore'
 
 import './app.css';
-import { wait } from '@testing-library/user-event/dist/utils';
 
 function App() {
-
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
 
+  const [posts, setPosts] = useState([]);
 
-  async function handleAdd() {
-    // await setDoc(doc(db, "posts", "12345"),{
 
-    // titulo: titulo,
-    //autor: autor,
-    //})
+  async function handleAdd(){
+    // await setDoc(doc(db, "posts", "12345"), {
+    //   titulo: titulo,
+    //   autor: autor,
+    // })
+    // .then(() => {
+    //   console.log("DADOS REGISTRADO NO BANCO!")
+    // })
+    // .catch((error) => {
+    //   console.log("GEROU ERRO" + error)
+    // }) 
 
-    //.then(() => {
-    //console.log("DADOS REGISTRADO NO BANCO!")
-
-    //})
-
-    //.catch((error) => {
-    //console.log("Gerou erro " + error)
-
-    //})
 
     await addDoc(collection(db, "posts"), {
       titulo: titulo,
-      autor:  autor,
+      autor: autor,
     })
-
     .then(() => {
-      console.log("DADOS REGISTRADO NO BANCO")
+      console.log("CADASTRADO COM SUCESSO")
       setAutor('');
-      setTitulo('');
+      setTitulo('')
+    })
+    .catch((error) => {
+      console.log("ERRO " + error)
     })
 
-    .catch((error) => {
-      console.log("Gerou erro" + error)
-    })
+
   }
 
 
-  async function buscarPosts(){
-   const postRef = doc(db, "posts", "12345")
+  async function buscarPost(){
+    // const postRef = doc(db, "posts", "vFvZAyFqebXFsFv0X89l")
+    // await getDoc(postRef)
+    // .then((snapshot) => {
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
 
-   await getDoc(postRef)
-   .then((snapshot) => {
-      setAutor(snapshot.data().autor)
-      setTitulo(snapshot.data().titulo)
-   })
-    .catch(() => {
-      console.log("ERRO AO BUSCAR")
+    // })
+    // .catch(()=>{
+    //   console.log("ERRO AO BUSCAR")
+    // })
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+      setPosts(lista);
+
     })
+    .catch((error) => {
+      console.log("DEU ALGUM ERRO AO BUSCAR")
+    })
+
+
   }
 
   return (
     <div>
-      <h1>ReactJs + Firebase</h1>
+      <h1>ReactJS + Firebase :)</h1>
 
-      <div className="container">
-        <label>Titulo: </label>
-        <textarea
-          type="text"
-          placeholder='digite o ttitulo'
-          value={titulo}
-          onChange={(e) => setTitulo(e.target.value)}
-        />
+    <div className="container">
+      <label>Titulo:</label>
+      <textarea 
+        type="text"
+        placeholder='Digite o titulo'
+        value={titulo}
+        onChange={ (e) => setTitulo(e.target.value) }
+      />
 
-        <label>Autor:</label>
-        <input
-          type="text"
-          placeholder="Autor do Post"
-          value={autor}
-          onChange={(e) => setAutor(e.target.value)}
-        />
+      <label>Autor:</label>
+      <input 
+        type="text" 
+        placeholder="Autor do post"
+        value={autor}
+        onChange={(e) => setAutor(e.target.value) }
+      />
 
-        <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={buscarPosts}>Buscar posts</button>
+      <button onClick={handleAdd}>Cadastrar</button>
+      <button onClick={buscarPost}>Buscar post</button>
 
-      </div>
+      <ul>
+        {posts.map( (post) => {
+          return(
+            <li key={post.id}>
+              <span>Titulo: {post.titulo} </span> <br/>
+              <span>Autor: {post.autor}</span> <br/>  <br/>
+            </li>
+          )
+        })}
+      </ul>
 
     </div>
 
+    </div>
   );
 }
 
